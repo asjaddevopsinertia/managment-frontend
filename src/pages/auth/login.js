f477597865bf0612564a649c8d28e5985f4cd716
ABT <abt@ABTs-Mac-mini.local>
2024-01-01 23:51:02 +0500

    Fixed Design issues

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -69,7 +69,7 @@ const Page = () => {
     <>
       <Head>
         <title>
-          Login | Devias Kit
+          Login
         </title>
       </Head>
       <Box
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,54 @@
-import Head from 'next/head';
-import { subDays, subHours } from 'date-fns';
-import { Box, Container, Unstable_Grid2 as Grid } from '@mui/material';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import { OverviewBudget } from 'src/sections/overview/overview-budget';
-import { OverviewLatestOrders } from 'src/sections/overview/overview-latest-orders';
-import { OverviewLatestProducts } from 'src/sections/overview/overview-latest-products';
-import { OverviewSales } from 'src/sections/overview/overview-sales';
-import { OverviewTasksProgress } from 'src/sections/overview/overview-tasks-progress';
-import { OverviewTotalCustomers } from 'src/sections/overview/overview-total-customers';
-import { OverviewTotalProfit } from 'src/sections/overview/overview-total-profit';
-import { OverviewTraffic } from 'src/sections/overview/overview-traffic';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Avatar, Card, CardContent, Stack, SvgIcon, Typography } from '@mui/material';
+import Head from "next/head";
+import { subDays, subHours } from "date-fns";
+import { Box, Container, Unstable_Grid2 as Grid } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import { OverviewBudget } from "src/sections/overview/overview-budget";
+import { OverviewLatestOrders } from "src/sections/overview/overview-latest-orders";
+import { OverviewLatestProducts } from "src/sections/overview/overview-latest-products";
+import { OverviewSales } from "src/sections/overview/overview-sales";
+import { OverviewTasksProgress } from "src/sections/overview/overview-tasks-progress";
+import { OverviewTotalCustomers } from "src/sections/overview/overview-total-customers";
+import { OverviewTotalProfit } from "src/sections/overview/overview-total-profit";
+import { OverviewTraffic } from "src/sections/overview/overview-traffic";
+import { useEffect, useState } from "react";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import axios from "axios";
+import { Avatar, Card, CardContent, Stack, SvgIcon, Typography } from "@mui/material";
+import dayjs, { Dayjs } from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('America/Los_Angeles');
 
 const now = new Date();
 
 const Page = () => {
+  const [selectedRange, setSelectedRange] = useState("week");
+  const [productData, setProductData] = useState([]);
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [tranformedData, setTransformedData] = useState([])
+  const [tranformedData1, setTransformedData1] = useState([])
 
-  const [selectedRange, setSelectedRange] = useState('week');
-  const [productData, setProductData] = useState([])
-  const [data, setData] = useState()
-  const [loading, setLoading] = useState(false)
+  const [endValue, setEndValue] =  useState(dayjs('2024-01-01'))
+  const [startValue, setStartValue] = useState(dayjs('2022-04-17'))
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true)
+      setLoading(true);
       try {
-        let token = window.localStorage.getItem('token');
+        let token = window.localStorage.getItem("token");
         const response = await axios.post(
-          'https://management.marmaladegrove.com/orders/time-range',
+          "https://management.marmaladegrove.com/orders/time-range",
           {
-            range: selectedRange,
+            range: "week",
           },
           {
             headers: {
@@ -43,14 +56,41 @@ const Page = () => {
             },
           }
         );
-        console.log("re", response.data)
-        setData(response.data)
-        setProductData(Object.keys(response.data.order.productDetails))
-        setLoading(false)
+        console.log("re", response.data);
+        setData(response.data);
+        setProductData(Object.keys(response.data.order.productDetails));
+        const transformedData = Object.keys(response.data.order.productDetails).reduce((result, productId) => {
+          const products = response.data.order.productDetails[61394944053];
+          Object.keys(products).forEach(productName => {
+            const { quantity, totalGrams } = products[productName];
+            result.push({
+              productName,
+              quantity,
+              grams: totalGrams
+            });
+          });
+          return result;
+        }, []);
+        setTransformedData(transformedData);
+
+        const transformedData1 = Object.keys(response.data.order.productDetails).reduce((result, productId) => {
+          const products = response.data.order.productDetails[65769406517];
+          Object.keys(products).forEach(productName => {
+            const { quantity, totalGrams } = products[productName];
+            result.push({
+              productName,
+              quantity,
+              grams: totalGrams
+            });
+          });
+          return result;
+        }, []);
+        setTransformedData1(transformedData1);
+        setLoading(false);
         // Do something with the response data here
       } catch (error) {
-        console.error('Error:', error);
-        setLoading(false)
+        console.error("Error:", error);
+        setLoading(false);
         // Handle errors here
       }
     };
@@ -62,190 +102,160 @@ const Page = () => {
     setSelectedRange(event.target.value);
   };
 
-  console.log("data",data?.order)
-  
+  console.log("data", data?.order);
 
+  console.log("start", dayjs(startValue))
+  console.log("start", dayjs(endValue))
+  console.log("transformed", tranformedData)
+  console.log("transformed1", tranformedData1)
   return (
     <>
       <Head>
-        <title>
-          Overview | Devias Kit
-        </title>
+        <title>Dashboard</title>
       </Head>
       <Box
         component="main"
         sx={{
           flexGrow: 1,
-          py: 8
+          py: 8,
         }}
       >
-
-
         <Container maxWidth="xl">
-
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Date</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={selectedRange}
-              label="Date"
-              onChange={handleChange}
-            >
-              <MenuItem value="week">Last 7 Days</MenuItem>
-              <MenuItem value="month">Last 30 Days</MenuItem>
-              <MenuItem value="start">Start Of Week</MenuItem>
-            </Select>
-          </FormControl>
-
-
-          <Typography variant="h4" sx={{ marginTop: '25px', marginBottom: '25px' }}>
+          <Grid container>
+            <Grid xs={1} sm={2} lg={3}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Date</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={selectedRange}
+                  label="Date"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="week">Last 7 Days</MenuItem>
+                  <MenuItem value="month">Last 30 Days</MenuItem>
+                  <MenuItem value="start">Start Of Week</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid xs={1} sm={2} lg={3}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <Box components={["DateTimePicker"]}>
+                  <DateTimePicker timezone="America/Los_Angeles" label="Basic date time picker" value={startValue}
+                  onChange={(newValue) => setStartValue(newValue)} />
+                </Box>
+              </LocalizationProvider>
+            </Grid>
+            <Grid xs={1} sm={2} lg={3}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <Box components={["DateTimePicker"]}>
+                  <DateTimePicker timezone="America/Los_Angeles" label="Basic date time picker" value={endValue}
+                  onChange={(newValue) => {
+                    console.log(newValue)
+                    setEndValue(newValue)}}/>
+                </Box>
+              </LocalizationProvider>
+            </Grid>
+          </Grid>
+          <Typography variant="h4" sx={{ marginTop: "25px", marginBottom: "25px" }}>
             Marmalade Grove
           </Typography>
 
-
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid
-              xs={12}
-              sm={6}
-              lg={3}
-            >
-
+          <Grid container spacing={3}>
+          <Grid xs={12} sm={6} lg={3}>
               <OverviewBudget
-                title="Total Orders Fulfilled"
+                title="Total Orders"
                 subTitle={selectedRange}
                 difference={12}
                 positive
-                sx={{ height: '100%' }}
-                value={data != undefined ? data?.order?.locationIdCounts[61394944053] : 'loading'}
+                sx={{ height: "100%" }}
+                value={data != undefined ? data?.order?.orders.length : "loading"}
+              />
+            </Grid>
+            <Grid xs={12} sm={6} lg={3}>
+              <OverviewBudget
+                title="Marmalade Grove"
+                subTitle={selectedRange}
+                difference={12}
+                positive
+                sx={{ height: "100%" }}
+                value={data != undefined ? data?.order?.locationIdCounts[61394944053] : "loading"}
+              />
+            </Grid>
+            <Grid xs={12} sm={6} lg={3}>
+              <OverviewBudget
+                title="Commerce"
+                subTitle={selectedRange}
+                difference={12}
+                positive
+                sx={{ height: "100%" }}
+                value={data != undefined ? data?.order?.locationIdCounts[65769406517] : "loading"}
               />
             </Grid>
 
-            {Array.isArray(productData) && Array.length > 0 ? productData.map(productId => {
-              console.log("product", productId)
-        const product = data.order.productDetails[61394944053];
-        return Object.keys(product).map(productName => {
-          console.log("productname", productName)
-          const { quantity, totalGrams } = product[productName];
-          return (
-            <Grid key={productName} item xs={12} sm={6} lg={3}>
+            <Grid xs={12} sm={6} lg={3}>
               <OverviewBudget
-                title={productName}
-                subTitle="Product"
-                difference={12} // Replace with the appropriate difference value
+                title="Total Order Value"
+                subTitle={selectedRange}
+                difference={12}
                 positive
-                sx={{ height: '100%' }}
-                value={`Quantity: ${quantity}, Grams: ${totalGrams}`}
+                sx={{ height: "100%" }}
+                value={data != undefined ? '$' + data?.order?.totalCurrentSubtotal : "loading"}
               />
             </Grid>
-          );
-        });
-      }): 'error'}
-            <Grid
-              xs={12}
-              sm={6}
-              lg={3}
-            >
+
+            <Grid xs={12} sm={6} lg={3}>
               <OverviewTotalCustomers
                 difference={16}
                 positive={false}
-                sx={{ height: '100%' }}
+                sx={{ height: "100%" }}
                 value="1.6k"
               />
             </Grid>
-            <Grid
-              xs={12}
-              sm={6}
-              lg={3}
-            >
-              <OverviewTasksProgress
-                sx={{ height: '100%' }}
-                value={75.5}
+            <Grid xs={12} sm={6} lg={3}>
+              <OverviewTasksProgress sx={{ height: "100%" }} value={75.5} />
+            </Grid>
+            <Grid xs={12} sm={6} lg={3}>
+              <OverviewTotalProfit sx={{ height: "100%" }} value="$15k" />
+            </Grid>
+          </Grid>
+          <Typography variant="h4" sx={{ marginTop: "25px", marginBottom: "25px" }}>
+            Commerce
+          </Typography>
+
+          <Grid container spacing={3}>
+            <Grid xs={12} sm={6} lg={3}>
+              <OverviewBudget
+                title="Total Fulfilled"
+                subTitle={selectedRange}
+                difference={12}
+                positive
+                sx={{ height: "100%" }}
+                value={data != undefined ? data?.order?.locationIdCounts[65769406517] : "loading"}
               />
             </Grid>
-            <Grid
-              xs={12}
-              sm={6}
-              lg={3}
-            >
-              <OverviewTotalProfit
-                sx={{ height: '100%' }}
-                value="$15k"
+
+            <Grid xs={12} sm={6} lg={3}>
+              <OverviewTotalCustomers
+                difference={16}
+                positive={false}
+                sx={{ height: "100%" }}
+                value="1.6k"
               />
             </Grid>
-        </Grid>
-            <Typography variant="h4" sx={{ marginTop: '25px', marginBottom: '25px' }}>
-              Commerce
-            </Typography>
-
-
-            <Grid
-              container
-              spacing={3}
-            >
-              <Grid
-                xs={12}
-                sm={6}
-                lg={3}
-              >
-
-                <OverviewBudget
-                  title="Total Fulfilled"
-                  subTitle={selectedRange}
-                  difference={12}
-                  positive
-                  sx={{ height: '100%' }}
-                  value={data != undefined ? data?.order?.locationIdCounts[65769406517] : 'loading'}
-                />
-              </Grid>
-
-              
-              <Grid
-                xs={12}
-                sm={6}
-                lg={3}
-              >
-                <OverviewTotalCustomers
-                  difference={16}
-                  positive={false}
-                  sx={{ height: '100%' }}
-                  value="1.6k"
-                />
-              </Grid>
-              <Grid
-                xs={12}
-                sm={6}
-                lg={3}
-              >
-                <OverviewTasksProgress
-                  sx={{ height: '100%' }}
-                  value={75.5}
-                />
-              </Grid>
-              <Grid
-                xs={12}
-                sm={6}
-                lg={3}
-              >
-                <OverviewTotalProfit
-                  sx={{ height: '100%' }}
-                  value="$15k"
-                />
-              </Grid>
+            <Grid xs={12} sm={6} lg={3}>
+              <OverviewTasksProgress sx={{ height: "100%" }} value={75.5} />
             </Grid>
+            <Grid xs={12} sm={6} lg={3}>
+              <OverviewTotalProfit sx={{ height: "100%" }} value="$15k" />
+            </Grid>
+          </Grid>
         </Container>
       </Box>
     </>
-  )
+  );
 };
 
-Page.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
-);
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default Page;
