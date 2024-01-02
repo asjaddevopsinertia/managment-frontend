7df3e5fe060b36e75c950fce5324b2108e20402c
ABT <abt@ABTs-Mac-mini.local>
2024-01-03 01:55:46 +0500

    Added tables

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,11 +23,40 @@ import { Avatar, Card, CardContent, Stack, SvgIcon, Typography } from "@mui/mate
 import dayjs, { Dayjs } from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
+import { CustomersTable } from "src/sections/customer/customers-table";
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault('America/Los_Angeles');
 
 const now = new Date();
+
+const columns = [
+  { id: 'product_name', label: 'Product Name', minWidth: 170 },
+  { id: 'type', label: 'Type', minWidth: 100 },
+  {
+    id: 'quantity',
+    label: 'Quantity',
+    minWidth: 170,
+    align: 'right',
+  },
+  {
+    id: 'grams',
+    label: 'Grams',
+    minWidth: 170,
+    align: 'right',
+
+  },
+];
+
+
 
 const Page = () => {
   const [selectedRange, setSelectedRange] = useState("week");
@@ -39,6 +68,41 @@ const Page = () => {
 
   const [endValue, setEndValue] =  useState(dayjs('2024-01-01'))
   const [startValue, setStartValue] = useState(dayjs('2022-04-17'))
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [rows, setRows] = useState([])
+  const [rows2, setRows2] = useState([])
+
+  function createData(product_name, type, quantity, grams) {
+    return { product_name, type, quantity, grams};
+  }
+  
+  // const rows = [
+  //   createData('India', 'IN', 1324171354, 3287263),
+  //   createData('China', 'CN', 1403500365, 9596961),
+  //   createData('Italy', 'IT', 60483973, 301340),
+  //   createData('United States', 'US', 327167434, 9833520),
+  //   createData('Canada', 'CA', 37602103, 9984670),
+  //   createData('Australia', 'AU', 25475400, 7692024),
+  //   createData('Germany', 'DE', 83019200, 357578),
+  //   createData('Ireland', 'IE', 4857000, 70273),
+  //   createData('Mexico', 'MX', 126577691, 1972550),
+  //   createData('Japan', 'JP', 126317000, 377973),
+  //   createData('France', 'FR', 67022000, 640679),
+  //   createData('United Kingdom', 'GB', 67545757, 242495),
+  //   createData('Russia', 'RU', 146793744, 17098246),
+  //   createData('Nigeria', 'NG', 200962417, 923768),
+  //   createData('Brazil', 'BR', 210147125, 8515767),
+  // ];
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -64,7 +128,8 @@ const Page = () => {
           Object.keys(products).forEach(productName => {
             const { quantity, totalGrams } = products[productName];
             result.push({
-              productName,
+              product_name:productName,
+              type: productName.includes('Fruit') ? 'Fruit' : 'Marmalade',
               quantity,
               grams: totalGrams
             });
@@ -78,13 +143,16 @@ const Page = () => {
           Object.keys(products).forEach(productName => {
             const { quantity, totalGrams } = products[productName];
             result.push({
-              productName,
+              product_name:productName,
+              type: productName.includes('Fruit') ? 'Fruit' : 'Marmalade',
               quantity,
               grams: totalGrams
             });
           });
           return result;
         }, []);
+        setRows(transformedData)
+        setRows2(transformedData1)
         setTransformedData1(transformedData1);
         setLoading(false);
         // Do something with the response data here
@@ -108,6 +176,7 @@ const Page = () => {
   console.log("start", dayjs(endValue))
   console.log("transformed", tranformedData)
   console.log("transformed1", tranformedData1)
+  console.log("rows", rows)
   return (
     <>
       <Head>
@@ -157,9 +226,7 @@ const Page = () => {
               </LocalizationProvider>
             </Grid>
           </Grid>
-          <Typography variant="h4" sx={{ marginTop: "25px", marginBottom: "25px" }}>
-            Marmalade Grove
-          </Typography>
+         
 
           <Grid container spacing={3}>
           <Grid xs={12} sm={6} lg={3}>
@@ -204,52 +271,118 @@ const Page = () => {
               />
             </Grid>
 
-            <Grid xs={12} sm={6} lg={3}>
-              <OverviewTotalCustomers
-                difference={16}
-                positive={false}
-                sx={{ height: "100%" }}
-                value="1.6k"
-              />
-            </Grid>
-            <Grid xs={12} sm={6} lg={3}>
-              <OverviewTasksProgress sx={{ height: "100%" }} value={75.5} />
-            </Grid>
-            <Grid xs={12} sm={6} lg={3}>
-              <OverviewTotalProfit sx={{ height: "100%" }} value="$15k" />
-            </Grid>
+
           </Grid>
+
+
           <Typography variant="h4" sx={{ marginTop: "25px", marginBottom: "25px" }}>
-            Commerce
+            Marmalade Grove
           </Typography>
 
-          <Grid container spacing={3}>
-            <Grid xs={12} sm={6} lg={3}>
-              <OverviewBudget
-                title="Total Fulfilled"
-                subTitle={selectedRange}
-                difference={12}
-                positive
-                sx={{ height: "100%" }}
-                value={data != undefined ? data?.order?.locationIdCounts[65769406517] : "loading"}
-              />
-            </Grid>
+          <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Array.isArray(rows) ? rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              }) : null}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {Array.isArray(rows) ? 
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      /> : null }
+    </Paper>
 
-            <Grid xs={12} sm={6} lg={3}>
-              <OverviewTotalCustomers
-                difference={16}
-                positive={false}
-                sx={{ height: "100%" }}
-                value="1.6k"
-              />
-            </Grid>
-            <Grid xs={12} sm={6} lg={3}>
-              <OverviewTasksProgress sx={{ height: "100%" }} value={75.5} />
-            </Grid>
-            <Grid xs={12} sm={6} lg={3}>
-              <OverviewTotalProfit sx={{ height: "100%" }} value="$15k" />
-            </Grid>
-          </Grid>
+
+    <Typography variant="h4" sx={{ marginTop: "25px", marginBottom: "25px" }}>
+            Commerce
+          </Typography>
+          
+          <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Array.isArray(rows2) ? rows2
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              }) : null}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {Array.isArray(rows2) ? 
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows2.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      /> : null }
+    </Paper>
         </Container>
       </Box>
     </>
